a60721256c19112f2ad40c4f45c0de8b341afce8
mariovuk <mariovuk@yahoo.com>
2017-09-18 13:12:41 +0200

    Unused class

--- app/src/main/java/com/naman14/timber/activities/NowPlayingActivity.java
+++ app/src/main/java/com/naman14/timber/activities/NowPlayingActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.StyleRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 
